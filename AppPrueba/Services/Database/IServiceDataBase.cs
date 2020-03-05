8263ac5e526e96531dc63d869cf103802ea2567b
German <germanc97@javerianacali.edu.co>
2020-03-05 12:49:54 -0500

    segunda clase

---
+++ AppPrueba/Services/Database/IServiceDataBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AppPrueba.Services.Database
+{
+    public interface IServiceDataBase<T> where T : class, new()
+    {
+        Task<List<T>>GetItemAsync();
+
+        Task<T> GetItemAsync(int id);
+
+        Task<int> InsertItemAsync(T item);
+
+        Task<int> UpdateItemAsync(T item);
+
+        Task<int> DeleteItemAsync(T item);
+    }
+
+}
---
+++ AppPrueba/Services/Database/IUserDataBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AppPrueba.Services.Database
+{
+    public interface IUserDataBase<T> : IServiceDataBase<T> where T: class, new()
+    {
+        Task<int> InsertAllItemsAsync(IList<T> items);
+
+        Task<int> DeleteAllItemsAsync();
+
+        Task<int> GetAllItemsAsync();
+        
+        Task InsertAllItemsWithChildrenAsync(IList<T> items);
+
+    }
+}
